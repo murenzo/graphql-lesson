da697378c910ea71202e04e0d64ba3b7aa15f9ab
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-07-21 00:21:21 +0100

    Implement Query using Apollo client to fetch data from graphQL api

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import Spinner from "../../components/spinner/spinner.component";
+import Collection from "./collection.component";
+
+const GET_COLLECTION_BY_TITLE = gql`
+    query getCollectionsByTitle($title: String!) {
+        getCollectionsByTitle(title: $title) {
+            id
+            title
+            items {
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`;
+
+const CollectionContainer = ({ match }) => (
+  <Query
+    query={GET_COLLECTION_BY_TITLE}
+    variables={{ title: match.params.collectionId }}
+  >
+    {({ loading, data }) => {
+      if (loading) return <Spinner />;
+      const { getCollectionsByTitle } = data;
+      return <Collection collection={getCollectionsByTitle} />;
+    }}
+  </Query>
+);
+
+export default CollectionContainer;
